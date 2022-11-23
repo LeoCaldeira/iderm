d5602953d5091748c974ba6d7cf9ab4eacd39d4f
Leonardo Bassini Caldeira <66039730+LeoCaldeira@users.noreply.github.com>
2022-11-22 21:24:50 -0300

    feat(about): ended the layot of the page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,8 +7,9 @@ const About = () => {
         <AuthLayout>
             <div className="about-wrapper">
                 <p>
-                    A iDerm é uma empresa de cuidados dermatológicos focada em atender o público utilizando o melhor da
-                    tecnologia e com isso conseguindo prestar com maior qualidade e o menor preço possível.
+                    A <span className="highlight"> iDerm</span> é uma empresa de cuidados dermatológicos focada em
+                    atender o público utilizando o melhor da tecnologia e com isso conseguindo prestar com maior
+                    qualidade e o menor preço possível.
                 </p>
                 <p>
                     Possuímos uma unidade fixa onde está instalada a infraestrutura principal para atender e dar suporte
@@ -24,15 +25,19 @@ const About = () => {
                     locais que esse tipo de serviço é de difícil acesso, devido à distância e valor do serviço.{' '}
                 </p>
 
-                <h5>
-                    Missão: Preservar a saúde, o a autoestima e a qualidade de vida das pessoas de baixa renda, a partir
-                    de um atendimento profissional, ético e personalizado.
-                </h5>
-                <h5>
-                    Visão: Manter e ampliar o nível da instituição e ser reconhecida na região como referência no
-                    tratamento de dermatológico em populações carentes.
-                </h5>
-                <h5>Valores: • Responsabilidade social • Tratamentos flexíveis</h5>
+                <p>
+                    <span>Missão:</span> Preservar a saúde, o a autoestima e a qualidade de vida das pessoas de baixa
+                    renda, a partir de um atendimento profissional, ético e personalizado.
+                </p>
+                <p>
+                    <span>Visão:</span> Manter e ampliar o nível da instituição e ser reconhecida na região como
+                    referência no tratamento de dermatológico em populações carentes.
+                </p>
+                <p>
+                    <span>Valores:</span>
+                    <li>Responsabilidade social</li>
+                    <li>Tratamentos flexíveis</li>
+                </p>
             </div>
         </AuthLayout>
     )
